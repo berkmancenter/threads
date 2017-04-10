e26af127344828a1432cf9e8fe4345ae295dd8d5
Minh Phan <wofi.minh@1pac.vn>
2017-04-10 17:30:37 +0700

    Fix seed file T_T

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 minh = User.create(username: 'Minh Phan', email: 'minh@xample.com', password: 'password')
 
-10.times.each do |_i|
+10.times.each do |i|
   r = Room.create(owner_id: minh.id, title: "Room #{i + 1}")
   RoomUser.create(room_id: r.id, user_id: minh.id)
   10.times.each { |m| Message.create(content: "message content #{m}", room_id: r.id, user_id: minh.id) }
