ea82acb58d4ac881c71219de81633abf323284bd
Minh Phan <wofi.minh@1pac.vn>
2017-04-10 17:28:40 +0700

    Remove faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 minh = User.create(username: 'Minh Phan', email: 'minh@xample.com', password: 'password')
 
 10.times.each do |_i|
-  r = Room.create(owner_id: minh.id, title: Faker::Name.name)
+  r = Room.create(owner_id: minh.id, title: "Room #{i + 1}")
   RoomUser.create(room_id: r.id, user_id: minh.id)
   10.times.each { |m| Message.create(content: "message content #{m}", room_id: r.id, user_id: minh.id) }
 end
