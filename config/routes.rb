a66ad4b10e5eed742f4cc32b81add938d9b51aa4
Minh Phan <wofi.minh@1pac.vn>
2017-04-12 15:24:01 +0700

    Add old message route

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,11 @@ Rails.application.routes.draw do
                              controller: 'users/registrations', as: :user_registration
   end
 
-  resources :rooms
-  resources :messages, only: %i(create destroy)
+  resources :rooms do
+    resources :messages, only: %i(create destroy) do
+      collection { get 'old/:first_message_id' => 'messages#old', constraints: { first_message_id: /\d+/ } }
+    end
+  end
 
   root to: 'rooms#index'
 
