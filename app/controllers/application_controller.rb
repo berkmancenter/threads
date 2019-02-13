4078f6c8714b7d8909ccbc5b9baa6443deb010ea
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-02-13 09:57:20 +0100

    Don't remove guest users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,6 @@ class ApplicationController < ActionController::Base
     if current_user
       if session[:guest_user_id] && session[:guest_user_id] != current_user.id
         logging_in
-        # reload guest_user to prevent caching problems before destruction
-        guest_user(with_retry = false).try(:reload).try(:destroy)
         session[:guest_user_id] = nil
       end
       current_user
--- app/models/room.rb
+++ app/models/room.rb
@@ -15,6 +15,7 @@ class Room < ApplicationRecord
   belongs_to :owner, class_name: User, foreign_key: :owner_id
   has_many :messages
   has_many :users, through: :messages
+  has_many :room_user_nicknames, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 1000 }
 
