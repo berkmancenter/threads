3b3853cf0d2055b40d7c05b3b89b387ee3526807
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-03-01 16:57:29 +0100

    Fix specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { is_expected.to belong_to(:avatar).class_name(Image).with_foreign_key(:avatar_id) }
     it { is_expected.to have_many(:messages) }
     it { is_expected.to have_many(:rooms).through(:messages) }
   end
