c2d7699b42b8fff9171a147fa92ad7037030b75b
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-04-05 14:59:48 +0200

    Seed user for each role

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(username: 'Adam Example', email: 'test@xample.com', password: 'password')
 
-10.times.each do |i|
-  r = Room.create(owner_id: user.id, title: "Room #{i + 1}")
-  RoomUser.create(room_id: r.id, user_id: user.id)
-  10.times.each { |m| Message.create(content: "message content #{m}", room_id: r.id, user_id: user.id) }
+# Seed users
+Role.all.each do |role|
+  user = User.create(
+    username: role.name,
+    email: role.name + '@example.com',
+    password: 'password',
+    roles: [role]
+  )
+  user.confirm
 end
