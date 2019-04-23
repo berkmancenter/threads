22f647d6acff34065c35d87cb33ab36b27e805da
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-04-23 11:12:17 +0200

    Set cookie domain from env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_chat_app_session'
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_threads_app_session',
+  domain: ENV['COOKIES_DOMAIN'] || nil
+)
