19332db9986c87251c2d9ddcbc5d55f20cdd17c4
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2019-09-18 20:05:31 +0200

    Make instance owners be able to see their private instances

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,7 @@ class Ability
 
     if user.role?(Role.anonymous) || user.role?(Role.owner) || user.role?(Role.moderator) || user.role?(Role.registered)
       can :read, Instance do |instance|
-        instance.private == false || instance.access_token == params[:access_token]
+        instance.private == false || instance.access_token == params[:access_token] || user == instance.owner
       end
       can :create_room_in, Instance do |instance|
         ((instance.moderators.include?(user) || user == instance.owner || !instance.closed)) && (instance.private == false || instance.access_token == params[:access_token])
@@ -28,7 +28,7 @@ class Ability
         user == instance.owner
       end
       can :read, Room do |room|
-        room.instance.private == false || room.instance.access_token == params[:access_token]
+        room.instance.private == false || room.instance.access_token == params[:access_token] || [room.instance.owner, room.owner].include?(user)
       end
       can :update, Room do |room|
         user == room.instance.owner || room.instance.moderators.include?(user)
