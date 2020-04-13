b1a54d404372f7d8d18ff116b0f3cde88861c58c
Peter Hankiewicz <peterh@cyber.harvard.edu>
2020-04-13 15:35:54 +0000

    Fix production deployment

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
+
 if Rails.env.staging? || Rails.env.production?
-  SMTP_SETTINGS = {
-    address: ENV['SMTP_ADDRESS'], # example: 'smtp.sendgrid.net'
+  unless defined?(SMTP_SETTINGS)
+    SMTP_SETTINGS = {
+    address: ENV['SMTP_ADDRESS'],
     authentication: :plain,
     enable_starttls_auto: false,
-    domain: ENV['SMTP_DOMAIN'], # example: 'this-app.com'
+    domain: ENV['SMTP_DOMAIN'],
     password: ENV['SMTP_PASSWORD'],
     port: ENV['SMTP_PORT'],
     user_name: ENV['SMTP_USERNAME']
-  } unless defined?( SMTP_SETTINGS )
+    }.freeze
+  end
+
+  Rails.application.config.action_mailer.smtp_settings = SMTP_SETTINGS
 end
